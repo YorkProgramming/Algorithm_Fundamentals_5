9aa0d565931a5af8d2010bf22fb2a641d9afdb2f
Matthew York <myorkprogramming@gmail.com>
2023-06-19 18:43:33 -0700

    Star Art

--- index.js
+++ index.js
@@ -44,3 +44,71 @@ function factorial(num){
 //multiplying each number to product
 
 //####################################################################################
+
+//3.
+//Star Art
+
+/* Assume that you have a text field that is exactly 75 characters long.
+You want to fill it with spaces and asterisks ('*'), sometimes called stars.
+You should print the given number of asterisks consecutively.
+Depending on which function is called, those stars should be left-justified
+(the first star would be very first char in the text field), or right-justified
+(the last star would be very last char in the text field, with potentially some
+number of spaces at beginning of text field before the block of stars start),
+or centered in the 75-character text field (with same number of spaces on either
+side of the block of stars, plus/minus one).
+
+Write a function drawLeftStars(num) that accepts a number and prints that many
+asterisks.
+
+Write a function drawRightStars(num) that prints 75 characters total. Stars should
+build from right side. The last num characters should be asterisks; the other 75
+should be spaces.
+
+Write function drawCenteredStars(num) that prints 75 characters total. The stars
+should be centered in the 75. The middle num characters should be asterisks; the
+rest of the 75 spaces. */
+
+function drawLeftStars(num){
+    
+        var stars = "";
+    
+        for(var i = 0; i < num; i++){
+            stars += "*";
+        }
+    
+        return stars;
+    }
+
+    console.log(drawLeftStars(500));
+
+    //We set stars to an empty string, then we loop through the numbers from 0 to num,
+    //adding a star to stars each time
+
+//####################################################################################
+
+function drawRightStars(num){
+    
+        var stars = "";
+    
+        for(var i = 0; i < 75; i++){
+            if(i < 75 - num){
+                stars += " ";
+            }
+            else{
+                stars += "*";
+            }
+        }
+    
+        return stars;
+    }
+
+    console.log(drawRightStars(25));
+
+    //We set stars to an empty string, then we loop through the numbers from 0 to 75,
+    //adding a star to stars each time, except when i is less than 75 - num, in which
+    //case we add a space to stars
+
+//####################################################################################
+
+
